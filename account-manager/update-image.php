220fa471dd20a302247c1d9c1b053153bceedb5e
DucPham2112 <94766118+MagicDinosaur@users.noreply.github.com>
2022-11-07 11:46:15 -0500

    Update update-image.php

--- account-manager/update-image.php
+++ account-manager/update-image.php
@@ -9,7 +9,7 @@ else{
 $img = $_POST['img'];
 // $date = date('Y-m-d H:i:s');
 $user_id = $_COOKIE["user_id"];
-$db = new PDO('mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_78c23a779fcb235', 'bb38448cc7530c', 'b83a2b78');
+$db = new PDO();
 $statement1 = $db->prepare("update user_info set avatar=:img where userID = :userID");
 
 $statement1->execute(array(':img' => $img, ':userID' => $user_id));
